9c8b544ab0b5941c79246d77135283f48ad9f0eb
Alex Yuly <alexyuly2@gmail.com>
2023-09-03 18:53:38 -0700

    CEnTAPIde syntax experiments

---
+++ examples/typescript-projects/todo-list/TodoListApp.ts
@@ -0,0 +1,65 @@
+import {
+  action,
+  can,
+  event,
+  field,
+  form,
+  input,
+  instance,
+  instanceOf,
+  label,
+  li,
+  listOf,
+  model,
+  prop,
+  renders,
+  store,
+  string,
+  valueOf,
+  view,
+  viewOf,
+} from "compendium";
+
+view(
+  "To-do List",
+  store("to-dos", listOf(instanceOf("To-do Item"))),
+  form({
+    onSubmit: action("to-dos").push(
+      field("To-do Item", {
+        text: event().get("data").get("text"),
+      })
+    ),
+    content: [],
+  })
+);
+
+view(
+  "To-do Item",
+  prop("model", instanceOf("To-do Item")),
+  li({
+    content: [
+      label({
+        content: [
+          input({
+            checked: valueOf("model").get("completed"),
+            type: "checkbox",
+          }),
+          valueOf("model").get("text"),
+        ],
+      }),
+    ],
+  })
+);
+
+model(
+  "To-do Item",
+  prop("text", string()),
+  store("completed", false),
+  can("complete", action("completed").enable()),
+  renders(
+    "view",
+    viewOf("To-do Item", {
+      model: instance(),
+    })
+  )
+);
