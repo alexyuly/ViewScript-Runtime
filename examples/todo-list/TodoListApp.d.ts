c25ca97fd9f43b527e2d24e59fd5057c5dbae8e0
Alex Yuly <alexyuly2@gmail.com>
2023-09-06 14:37:04 -0700

    WIP on Many, Method, Generator (needs work)

--- examples/todo-list/TodoListApp.d.ts
+++ examples/todo-list/TodoListApp.d.ts
@@ -2,12 +2,13 @@ import type {
   BooleanModel,
   Dispatch,
   Let,
-  Many,
+  // Many,
   Model,
-  Show,
+  // NumberModel
+  // Show,
   StringModel,
   Take,
-  View,
+  // View,
 } from "../../runtime/lib";
 
 type TodoItemModel = Model<
@@ -15,51 +16,51 @@ type TodoItemModel = Model<
   {
     text: Take<StringModel>;
     completed: Let<BooleanModel, false>;
-    complete(): Dispatch<TodoItem, "completed", "enable">;
+    complete(): Dispatch<TodoItemModel, "completed", "enable">;
   }
 >;
 
-type TodoItemView = View<
-  "TodoItemView",
-  {
-    model: Take<TodoItemModel>;
-  },
-  Show<
-    "li",
-    TodoItemView,
-    {
-      click: Dispatch<TodoItemView, "model", "complete">;
-      padding: "8px 16px";
-      display: "flex";
-      "align-items": "center";
-    },
-    Show<
-      "label",
-      TodoItemView,
-      {},
-      [] // TODO
-    >
-  >
->;
+// type TodoItemView = View<
+//   "TodoItemView",
+//   {
+//     model: Take<TodoItemModel>;
+//   },
+//   Show<
+//     "li",
+//     TodoItemView,
+//     {
+//       click: Dispatch<TodoItemView, "model", "complete">;
+//       padding: "8px 16px";
+//       display: "flex";
+//       "align-items": "center";
+//     },
+//     Show<
+//       "label",
+//       TodoItemView,
+//       {},
+//       [] // TODO
+//     >
+//   >
+// >;
 
-export type TodoListApp = View<
-  "TodoListApp",
-  {
-    model: Let<Many<TodoItemModel>>;
-  },
-  Show<
-    "main",
-    TodoListApp,
-    {
-      position: "fixed";
-      width: "100%";
-      height: "100%";
-      padding: "32px";
-      font: "16px sans-serif";
-      display: "flex";
-      "flex-direction": "column";
-      "align-items": "center";
-    },
-    [] // TODO
-  >
->;
+// export type TodoListApp = View<
+//   "TodoListApp",
+//   {
+//     model: Let<Many<TodoItemModel>>;
+//   },
+//   Show<
+//     "main",
+//     TodoListApp,
+//     {
+//       position: "fixed";
+//       width: "100%";
+//       height: "100%";
+//       padding: "32px";
+//       font: "16px sans-serif";
+//       display: "flex";
+//       "flex-direction": "column";
+//       "align-items": "center";
+//     },
+//     [] // TODO
+//   >
+// >;
--- runtime/lib/index.ts
+++ runtime/lib/index.ts
@@ -1,4 +1,14 @@
-export { BooleanModel, Dispatch, Let, Model, StringModel, Take };
+export {
+  BooleanModel,
+  Dispatch,
+  Generate,
+  Let,
+  Many,
+  Model,
+  NumberModel,
+  StringModel,
+  Take,
+};
 
 type Action<M extends Model | void = void> = (
   parameter: M extends void ? never : M
@@ -25,6 +35,8 @@ type ComplexDataType<M extends Model> = {
 
 type DataType<M extends Model> = M extends BooleanModel
   ? boolean
+  : M extends NumberModel
+  ? number
   : M extends StringModel
   ? string
   : M extends Model
@@ -33,17 +45,8 @@ type DataType<M extends Model> = M extends BooleanModel
 
 type Dispatch<
   M extends Model,
-  F extends {
-    [K in keyof M["properties"]]: M["properties"][K] extends Field ? K : never;
-  }[keyof M["properties"]],
-  A extends {
-    [K in keyof ModelChild<M, F>["properties"]]: ModelChild<
-      M,
-      F
-    >["properties"][K] extends Action
-      ? K
-      : never;
-  }[keyof ModelChild<M, F>["properties"]],
+  F extends ModelField<M>,
+  A extends ModelAction<M, F>,
 > = Node<"Dispatch"> &
   Field & {
     field: F;
@@ -54,17 +57,53 @@ type Field<M extends Model = Model> = {
   reference: Reference<M>;
 };
 
+type Generate<
+  M extends Model,
+  F extends ModelField<M>,
+  E extends ModelMethod<M, F>,
+> = Node<"Generate"> &
+  Field & {
+    field: F;
+    method: E;
+  };
+
 type Let<M extends Model, D extends DataType<M> | void = void> = Node<"Let"> &
   Field<M> & {
     value: D extends void ? never : D;
   };
 
+type Many<M extends Model> = PrimitiveModel<
+  "Many",
+  {
+    length(): NumberModel;
+    push(parameter: M): void;
+  }
+> & {
+  model: Reference<M>;
+};
+
+type Method<R extends Model = Model, M extends Model | void = void> = (
+  parameter: M extends void ? never : M
+) => R;
+
 type Model<
   K extends string = string,
-  P extends Record<string, Action | Field> = {},
+  P extends Record<string, Action | Field | Method> = {},
 > = NodeOfKind<"Model", K> & {
   properties: P;
 };
+
+type ModelAction<
+  M extends Model = Model,
+  F extends ModelField<M> = ModelField<M>,
+> = {
+  [K in keyof ModelChild<M, F>["properties"]]: ModelChild<
+    M,
+    F
+  >["properties"][K] extends Action
+    ? K
+    : never;
+}[keyof ModelChild<M, F>["properties"]];
 
 type ModelChild<
   M extends Model,
@@ -75,6 +114,22 @@ type ModelChild<
     : never
   : never;
 
+type ModelField<M extends Model = Model> = {
+  [K in keyof M["properties"]]: M["properties"][K] extends Field ? K : never;
+}[keyof M["properties"]];
+
+type ModelMethod<
+  M extends Model = Model,
+  F extends ModelField<M> = ModelField<M>,
+> = {
+  [K in keyof ModelChild<M, F>["properties"]]: ModelChild<
+    M,
+    F
+  >["properties"][K] extends Method
+    ? K
+    : never;
+}[keyof ModelChild<M, F>["properties"]];
+
 type Node<N extends string> = {
   name: N;
 };
@@ -83,9 +138,11 @@ type NodeOfKind<N extends string, K extends string> = Node<N> & {
   kind: K;
 };
 
+type NumberModel = PrimitiveModel<"NumberModel">;
+
 type PrimitiveModel<
   K extends string = string,
-  P extends Record<string, Action> = {},
+  P extends Record<string, Action | Method> = {},
 > = Model<K, P> & {
   properties: P;
 };
