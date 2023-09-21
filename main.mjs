37347eb3dad134dc739461044ebb6557c236eee3
Alex Yuly <alexyuly2@gmail.com>
2023-09-20 21:17:39 -0700

    Parse strings of text

--- main.mjs
+++ main.mjs
@@ -18,24 +18,28 @@ function main() {
   console.log(JSON.stringify(fileLines, null, 2));
   console.log();
 
-  const fileSplitLines = fileLines.map((line) => line.split(" "));
-  console.log("fileSplitLines:");
-  console.log(JSON.stringify(fileSplitLines, null, 2));
+  const file = fileLines.map((line) => line.split(" "));
+  console.log("file:");
+  console.log(JSON.stringify(file, null, 2));
   console.log();
 
-  for (let i = 0; i < fileSplitLines.length; i++) {
-    const line = fileSplitLines[i];
+  for (let L = 0; L < file.length; L++) {
+    const line = file[L];
+    console.log(`Line L = ${line}`);
 
     let consecutiveEmptyWords = 0;
-    let isInsideString = false;
+    let consecutiveTextParts = 0;
 
-    for (let j = 0; j < line.length; j++) {
-      const word = line[j];
+    for (let W = 0; W < line.length; W++) {
+      const word = line[W];
+      console.log(`Word W = ${word}`);
+
+      let wOffset = 0;
 
       if (word === "") {
         if (line.length === 1) {
           // This is an empty line.
-          fileSplitLines.splice(i, 1, { empty: true });
+          file.splice(L, 1, { empty: true });
         } else {
           consecutiveEmptyWords++;
         }
@@ -43,23 +47,76 @@ function main() {
         if (consecutiveEmptyWords > 0) {
           assert(
             consecutiveEmptyWords % indentationSpacing === 0,
-            `Invalid indentation detected at line ${i}. Indentation must be in multiples of 3 spaces.`
+            `Invalid indentation is at line ${L}. Indentation must be in multiples of 3 spaces.`
           );
+
+          console.log("consecutiveEmptyWords", consecutiveEmptyWords);
 
           const indentationLevel = consecutiveEmptyWords / indentationSpacing;
 
-          line.splice(j - consecutiveEmptyWords, consecutiveEmptyWords, {
+          line.splice(W - consecutiveEmptyWords, consecutiveEmptyWords, {
             indentationLevel,
           });
+
+          wOffset += consecutiveEmptyWords - 1;
+
+          consecutiveEmptyWords = 0;
         }
 
-        // TODO process strings
+        if (word[0] === '"') {
+          assert(
+            consecutiveTextParts === 0 || word.length === 1,
+            `Invalid character is at line ${L}, word ${W}. Space must come after closing quote of text.`
+          );
+        } else if (
+          word[word.length - 1] === '"' &&
+          word[word.length - 2] !== "\\"
+        ) {
+          assert(
+            consecutiveTextParts > 0,
+            `Invalid character detected at line ${L}, word ${W}. Space must come before opening quote of text.`
+          );
+        }
+
+        if (word[0] === '"' && consecutiveTextParts === 0) {
+          if (word[word.length - 1] === '"' && word[word.length - 2] !== "\\") {
+            line.splice(W - consecutiveTextParts, consecutiveTextParts, {
+              text: word.slice(1, word.length - 1),
+            });
+          } else {
+            consecutiveTextParts++;
+          }
+        } else if (word[0] === '"' || word[word.length - 1] === '"') {
+          console.log("consecutiveTextParts", consecutiveTextParts);
+          console.log(line.slice(W - consecutiveTextParts, W + 1));
+          line.splice(W - consecutiveTextParts, consecutiveTextParts + 1, {
+            text: line
+              .slice(W - consecutiveTextParts, W + 1)
+              .reduce(
+                (r, x, i, a) =>
+                  r +
+                  (i === 0
+                    ? x.slice(1) + " "
+                    : i === a.length - 1
+                    ? x.slice(0, x.length - 1)
+                    : x + " "),
+                ""
+              ),
+          });
+
+          wOffset += consecutiveTextParts - 1;
+
+          consecutiveTextParts = 0;
+        }
       }
+
+      W -= wOffset;
     }
   }
 
-  console.log("fileSplitLines:");
-  console.log(JSON.stringify(fileSplitLines, null, 2));
+  console.log();
+  console.log("file:");
+  console.log(JSON.stringify(file, null, 2));
   console.log();
 }
 
