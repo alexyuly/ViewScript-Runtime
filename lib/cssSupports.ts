33190e4e1c7d5eca88aee9847af8122d8bd86674
Alex Yuly <alexyuly2@gmail.com>
2023-09-28 16:48:18 -0700

    Add cursor style support

--- lib/cssSupports.ts
+++ lib/cssSupports.ts
@@ -1,4 +1,4 @@
-const cssPropertyNames = ["background", "color", "font", "padding"];
+const cssPropertyNames = ["background", "color", "cursor", "font", "padding"];
 
 export const cssSupports = (propertyName: string) =>
   cssPropertyNames.includes(propertyName);
