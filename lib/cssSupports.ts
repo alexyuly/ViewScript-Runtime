5e543584c9ee9dc71ff405d456d5250a3de74fc3
Alex Yuly <alexyuly2@gmail.com>
2023-09-28 11:33:58 -0700

    Prepping to publish

--- lib/cssSupports.ts
+++ lib/cssSupports.ts
@@ -2,6 +2,3 @@ const cssPropertyNames = ["background", "color", "font", "padding"];
 
 export const cssSupports = (propertyName: string) =>
   cssPropertyNames.includes(propertyName);
-
-// TODO Consider using CSS.Supports as part of this implementation.
-// For now, it seems faster and more stable to use a constant list of available properties.
