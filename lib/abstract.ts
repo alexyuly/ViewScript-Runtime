43ba8c28d94bdc46c79d899e65a0aa29c153c66a
Alex Yuly <alexyuly2@gmail.com>
2023-11-05 13:34:32 -0800

    WIP on abstract.ts

--- lib/abstract.ts
+++ lib/abstract.ts
@@ -4,6 +4,8 @@ export type App = Node<"app"> & {
   renders: Component;
 };
 
+/* Tier 1 */
+
 export type Node<Kind extends string> = {
   kind: Kind;
 };
@@ -13,42 +15,81 @@ export type Model<Name extends string = string> = Node<"model"> &
     members: Record<string, Model | Field | Method | Action | ((argument: any) => unknown)>;
   };
 
+export type View<Name extends string = string> = Node<"view"> &
+  Called<Name> & {
+    fields: Record<string, Field>;
+    streams: Record<`on${string}`, Stream>;
+    renders: Component;
+  };
+
+export type Component = Node<"component"> & {
+  renders: Feature | Landscape;
+};
+
+/* Tier 2 */
+
 export type Called<Name extends string = string> = {
   name: Name;
 };
 
 export type Field<M extends Model = Model> = Node<"field"> & {
-  channel: Store<M> | Binding<M> | Slot<M> | Option<M> | FieldPointer<M> | MethodPointer<M>;
+  channel:
+    | Value<M>
+    | FieldPlan<M>
+    | FieldPointer<M>
+    | MethodPointer<M>
+    | Option<M>
+    | Store<M>
+    | WritableFieldPlan<M>
+    | WritableFieldPointer<M>;
 };
 
 export type Method<
   M extends Model = Model,
   P extends Model | null = Model | null,
 > = Node<"method"> & {
-  parameter: P extends Model ? Called & ReadOnlyField<P> : never;
-  result: ReadOnlyField<M>;
+  parameter: P extends Model ? Called & Field<P> : never;
+  result: Field<M>;
 };
 
 export type Action<M extends Model | null = Model | null> = Node<"action"> & {
-  parameter: M extends Model ? Called & ReadOnlyField<M> : never;
+  parameter: M extends Model ? Called & Field<M> : never;
   steps: Array<ActionPointer | Exception | StreamPointer>;
 };
 
-export type Store<M extends Model> = Node<"store"> &
-  Modeled<M> & {
-    value: Value<M>;
+export type Stream<M extends Model | null = Model | null> = Node<"stream"> & Modeled<M>;
+
+export type Feature = Node<"feature"> & {
+  tagName: string;
+  properties: Record<string, Field>;
+  reactions: Record<`on${string}`, Action>;
+};
+
+export type Landscape<M extends View = View> = Node<"landscape"> & {
+  viewName: M["name"];
+  properties: {
+    [Key in keyof M["fields"]]?: Property<M["fields"][Key]>;
   };
-
-export type Binding<M extends Model> = Node<"binding"> & Modeled<M>;
-
-export type Slot<M extends Model> = Node<"slot"> & Modeled<M>;
-
-export type Option<M extends Model> = Node<"option"> &
-  Modeled<M> & {
-    condition: Field<Model<"Boolean">>;
-    result: Field<M>;
-    opposite: Field<M>;
+  reactions: {
+    [Key in keyof M["streams"]]?: M["streams"][Key] extends Stream<infer P> ? Action<P> : never;
   };
+};
+
+/* Tier 3 */
+
+export type Value<M extends Model = Model> = M["name"] extends "Array"
+  ? Array<Field>
+  : M["name"] extends "Boolean"
+  ? boolean
+  : M["name"] extends "Number"
+  ? number
+  : M["name"] extends "String"
+  ? string
+  : M["name"] extends "Component"
+  ? Component
+  : Array<Field> | boolean | number | string | Component | Structure<M>;
+
+export type FieldPlan<M extends Model> = Node<"fieldPlan"> & Modeled<M>;
 
 export type FieldPointer<M extends Model> = Node<"fieldPointer"> &
   Modeled<M> & {
@@ -66,9 +107,24 @@ export type MethodPointer<
     argument: P extends Model ? Field<P> : never;
   };
 
-export type ReadOnlyField<M extends Model> = Node<"field"> & {
-  channel: Slot<M> | Option<M> | FieldPointer<M> | MethodPointer<M>;
-};
+export type Option<M extends Model> = Node<"option"> &
+  Modeled<M> & {
+    condition: Field<Model<"Boolean">>;
+    result: Field<M>;
+    opposite: Field<M>;
+  };
+
+export type Store<M extends Model> = Node<"store"> &
+  Modeled<M> & {
+    value: Value<M>;
+  };
+
+export type WritableFieldPlan<M extends Model> = Node<"writableFieldPlan"> & Modeled<M>;
+
+export type WritableFieldPointer<M extends Model> = Node<"writableFieldPointer"> &
+  Modeled<M> & {
+    fieldPath: Array<string>;
+  };
 
 export type ActionPointer<M extends Model | null = Model | null> = Node<"actionPointer"> & {
   actionPath: Array<string>;
@@ -89,17 +145,13 @@ export type Modeled<M extends Model | null> = {
   modelName: M extends Model ? M["name"] : never;
 };
 
-export type Value<M extends Model = Model> = M["name"] extends "Array"
-  ? Array<Field>
-  : M["name"] extends "Boolean"
-  ? boolean
-  : M["name"] extends "Number"
-  ? number
-  : M["name"] extends "String"
-  ? string
-  : M["name"] extends "Component"
-  ? Component
-  : Array<Field> | boolean | number | string | Component | Structure<M>;
+export type Property<M extends Field> = M["channel"] extends WritableFieldPlan<infer P>
+  ? WritableField<P>
+  : M["channel"] extends FieldPlan<infer P>
+  ? Field<P>
+  : never;
+
+/* Tier 4 */
 
 export type Structure<M extends Model = Model> = Node<"structure"> &
   Modeled<M> & {
@@ -110,39 +162,6 @@ export type Structure<M extends Model = Model> = Node<"structure"> &
     };
   };
 
-export type Property<M extends Field> = M["channel"] extends Binding<infer Class>
-  ? Field<Class>
-  : M["channel"] extends Slot<infer Class>
-  ? ReadOnlyField<Class>
-  : never;
-
-export type View<Name extends string = string> = Node<"view"> &
-  Called<Name> & {
-    fields: Record<string, Field>;
-    streams: Record<`on${string}`, Stream>;
-    renders: Component;
-  };
-
-export type Stream<M extends Model | null = Model | null> = Node<"stream"> & Modeled<M>;
-
-export type Component = Node<"component"> & {
-  renders: Feature | Landscape;
-};
-
-export type Feature = Node<"feature"> & {
-  tagName: string;
-  properties: Record<string, Field>;
-  reactions: Record<`on${string}`, Action>;
-};
-
-export type Landscape<M extends View = View> = Node<"landscape"> & {
-  viewName: M["name"];
-  properties: {
-    [Key in keyof M["fields"]]?: Property<M["fields"][Key]>;
-  };
-  reactions: {
-    [Key in keyof M["streams"]]?: M["streams"][Key] extends Stream<infer Parameter>
-      ? Action<Parameter>
-      : never;
-  };
+export type WritableField<M extends Model> = Node<"field"> & {
+  channel: Store<M> | WritableFieldPlan<M> | WritableFieldPointer<M>;
 };
