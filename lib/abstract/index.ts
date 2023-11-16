f2200e32f8ce678013090f20eb4912083619b152
Alex Yuly <alexyuly2@gmail.com>
2023-11-15 23:13:25 -0800

    Simplifying abstract and reconciling structures (wip)

--- lib/abstract/index.ts
+++ lib/abstract/index.ts
@@ -14,19 +14,16 @@ export type Node<Kind extends string = string> = {
   kind: Kind;
 };
 
-export type Model<Name extends string = string> = Node<"model"> &
-  Named<Name> & {
-    fields: Record<string, Field>;
-    methods: Record<string, Method>;
-    actions: Record<string, Action>;
-  };
+export type Model = Node<"model"> & {
+  name: string;
+  scope: Record<string, Field | Method | Action>;
+};
 
-export type View<Name extends string = string> = Node<"view"> &
-  Named<Name> & {
-    fields: Record<string, Field>;
-    streams: Record<string, Stream>;
-    renderable: Renderable;
-  };
+export type View = Node<"view"> & {
+  name: string;
+  scope: Record<string, Stream | Field>;
+  renderable: Renderable;
+};
 
 export type Renderable = Node<"renderable"> & {
   element: Feature | Landscape;
@@ -34,106 +31,89 @@ export type Renderable = Node<"renderable"> & {
 
 /* Tier 2 */
 
-export type Named<Name extends string = string> = {
-  name: Name;
+export type Field = Node<"field"> & {
+  publisher: Parameter | Store | Switch | Pointer | MethodCall;
 };
 
-export type Field<M extends Model = Model> = Node<"field"> & {
-  publisher: Parameter<M> | Store<M> | Switch<M> | Pointer<M> | MethodCall<M>;
+export type Method = Node<"method"> & {
+  parameter?: Parameter & { name: string };
+  result: Field;
 };
 
-export type Method<
-  M extends Model = Model,
-  P extends Model | null = Model | null,
-> = Node<"method"> & {
-  parameter?: P extends Model ? Named & Parameter<P> : never;
-  result: Field<M>;
-};
-
-export type Action<M extends Model | null = Model | null> = Node<"action"> & {
-  parameter?: M extends Model ? Named & Parameter<M> : never;
+export type Action = Node<"action"> & {
+  parameter?: Parameter & { name: string };
   steps: Array<ActionCall | StreamCall | Exception>;
 };
 
-export type Stream<M extends Model | null = Model | null> = Node<"stream"> & Modeled<M>;
+export type Stream = Node<"stream"> & {
+  modelName?: string;
+};
 
 export type Feature = Node<"feature"> & {
   tagName: string;
-  properties: Record<string, Field>;
-  reactions: Record<string, Action>;
+  properties: Record<string, Field | Action>;
 };
 
 export type Landscape<V extends View = View> = Node<"landscape"> & {
   viewName: V["name"];
-  properties: Partial<V["fields"]>;
-  reactions: {
-    [Key in keyof V["streams"]]?: V["streams"][Key] extends Stream<infer M> ? Action<M> : never;
-  };
+  properties: Record<string, Field | Action>;
 };
 
 /* Tiers 3 and greater */
 
-export type Parameter<M extends Model = Model> = Node<"parameter"> & Modeled<M>;
-
-export type Store<M extends Model = Model> = Node<"store"> &
-  Modeled<M> & {
-    value: M["name"] extends "Array"
-      ? Array<Field>
-      : M["name"] extends "Boolean"
-      ? boolean
-      : M["name"] extends "Number"
-      ? number
-      : M["name"] extends "String"
-      ? string
-      : M["name"] extends "Renderable"
-      ? Renderable
-      : unknown;
-  };
-
-export type Switch<M extends Model = Model> = Node<"switch"> &
-  Modeled<M> & {
-    condition: Field<Model<"Boolean">>;
-    positive: Field<M>;
-    negative: Field<M>;
-  };
-
-export type Pointer<M extends Model = Model> = Node<"pointer"> &
-  Modeled<M> & {
-    scope?: MethodCall;
-    address: Array<string>;
-  };
-
-export type MethodCall<
-  M extends Model = Model,
-  P extends Model | null = Model | null,
-> = Node<"methodCall"> &
-  Modeled<M> & {
-    scope?: MethodCall;
-    address: Array<string>;
-    argument?: P extends Model ? Field<P> : never;
-  };
-
-export type ActionCall<M extends Model | null = Model | null> = Node<"actionCall"> & {
-  address: Array<string>;
-  argument?: M extends Model ? Field<M> : never;
+export type Parameter = Node<"parameter"> & {
+  modelName: string;
 };
 
-export type StreamCall<M extends Model | null = Model | null> = Node<"streamCall"> &
-  Modeled<M> &
-  Named & {
-    output: M extends Model ? Field<M> : never;
-  };
+export type Store = Node<"store"> & {
+  value: Value;
+};
+
+export type Switch = Node<"switch"> & {
+  modelName: string;
+  condition: Field;
+  positive: Field;
+  negative: Field;
+};
+
+export type Pointer = Node<"pointer"> & {
+  modelName: string;
+  base?: MethodCall;
+  fieldAddress: Array<string>;
+};
+
+export type MethodCall = Node<"methodCall"> & {
+  modelName: string;
+  base?: MethodCall;
+  methodAddress: Array<string>;
+  argument?: Field;
+};
+
+export type ActionCall = Node<"actionCall"> & {
+  actionAddress: Array<string>;
+  argument?: Field;
+};
+
+export type StreamCall = Node<"streamCall"> & {
+  modelName?: string;
+  streamName: string;
+  output?: Field;
+};
 
 export type Exception = Node<"exception"> & {
-  condition: Field<Model<"Boolean">>;
+  condition: Field;
   steps?: Array<ActionCall | StreamCall | Exception>;
 };
 
-export type Modeled<M extends Model | null> = {
-  modelName: M extends Model ? M["name"] : never;
+export type Value = Node<"value"> & {
+  element: Part | Renderable | Structure;
 };
 
-export type Structure<M extends Model = Model> = Node<"structure"> &
-  Modeled<M> & {
-    properties: Partial<M["fields"]>;
-  };
+export type Part = Node<"part"> & {
+  data: unknown;
+};
+
+export type Structure = Node<"structure"> & {
+  modelName: string;
+  properties: Record<string, Field>;
+};
