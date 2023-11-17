4a3ea8eb9e8fc56e417fef1ab54b4d611bc3d5d4
Alex Yuly <alexyuly2@gmail.com>
2023-11-17 10:23:25 -0800

    Let views have methods and acitons; add doc comments

--- lib/abstract/index.ts
+++ lib/abstract/index.ts
@@ -18,16 +18,22 @@ export type Model = {
 
 export type View = {
   kind: "view";
-  scope: Record<string, Field | Stream>;
+  scope: Record<string, Field | Method | Action | Stream>;
   render: Feature | Landscape;
 };
 
+/**
+ * Represents an HTML element.
+ */
 export type Feature = {
   kind: "feature";
   tagName: string;
   properties: Record<string, Field | Action>;
 };
 
+/**
+ * Represents an instance of a view.
+ */
 export type Landscape = {
   kind: "landscape";
   viewName: string;
@@ -36,23 +42,35 @@ export type Landscape = {
 
 /* Tier III */
 
+/**
+ * Publishes content from a single store, directly or indirectly.
+ */
 export type Field = {
   kind: "field";
   publisher: Parameter | Store | Switch | FieldCall | MethodCall;
 };
 
+/**
+ * Publishes content from a new field to each subscriber.
+ */
 export type Method = {
   kind: "method";
   parameter?: Parameter & { name: string };
   result: Field;
 };
 
+/**
+ * Subscribes to events and handles them.
+ */
 export type Action = {
   kind: "action";
   parameter?: Parameter & { name: string };
   steps: Array<ActionCall | StreamCall | Exception>;
 };
 
+/**
+ * Subscribes to events and forwards them.
+ */
 export type Stream = {
   kind: "stream";
   parameter?: Parameter;
@@ -60,16 +78,25 @@ export type Stream = {
 
 /* Tier IV */
 
+/**
+ * Represents a placeholder for a value.
+ */
 export type Parameter = {
   kind: "parameter";
   modelName: string;
 };
 
+/**
+ * Maintains a value while the app is running.
+ */
 export type Store = {
   kind: "store";
   content: Feature | Landscape | Part | Structure;
 };
 
+/**
+ * Conditionally selects from two fields.
+ */
 export type Switch = {
   kind: "switch";
   condition: Field;
@@ -77,12 +104,18 @@ export type Switch = {
   negative?: Field;
 };
 
+/**
+ * Forwards content from a field.
+ */
 export type FieldCall = {
   kind: "fieldCall";
   scope?: Field;
   name: string;
 };
 
+/**
+ * Forwards content from a method.
+ */
 export type MethodCall = {
   kind: "methodCall";
   scope?: Field;
@@ -90,6 +123,9 @@ export type MethodCall = {
   argument?: Field;
 };
 
+/**
+ * Forwards events to an action.
+ */
 export type ActionCall = {
   kind: "actionCall";
   scope?: Field;
@@ -97,12 +133,18 @@ export type ActionCall = {
   argument?: Field;
 };
 
+/**
+ * Forwards events to a stream.
+ */
 export type StreamCall = {
   kind: "streamCall";
   name: string;
   argument?: Field;
 };
 
+/**
+ * Conditionally performs steps and exits the current action.
+ */
 export type Exception = {
   kind: "exception";
   condition: Field;
@@ -111,11 +153,17 @@ export type Exception = {
 
 /* Tier V */
 
+/**
+ * Represents a value that is not associated with a model.
+ */
 export type Part = {
   kind: "part";
   value: unknown;
 };
 
+/**
+ * Represents an instance of a model.
+ */
 export type Structure = {
   kind: "structure";
   modelName: string;
