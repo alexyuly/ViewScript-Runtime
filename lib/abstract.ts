62066cb8d4a009412a736af2440fa7c1006f0a65
Alex Yuly <alexyuly2@gmail.com>
2024-03-15 12:28:53 -0700

    WIP refactoring

--- lib/abstract.ts
+++ lib/abstract.ts
@@ -13,38 +13,47 @@ export namespace Abstract {
 
   export type Constant = Node<"constant"> & {
     parameter: Parameter;
-    binding: Data | Ref | Call | Quest | List | Structure | Action | View | Component;
+    binding: Entity;
   };
 
   export type Variable = Node<"variable"> & {
     parameter: Parameter;
-    binding: Data | Ref | Call | Quest | List | Structure | Action | View | Component;
+    binding: Entity;
   };
 
   export type Parameter = Node<"parameter"> & {
     name: string;
   };
 
-  export type Data = Node<"data"> & {
-    value: any;
+  export type Entity = Node<"entity"> & {
+    binding: Ref | Call | Expression | Quest | Data | List | Structure | Action | View | Component;
   };
 
   export type Ref = Node<"ref"> & {
-    scope: Data | Ref | Call | Quest | List | Structure | Action | View | Component;
+    scope: Entity;
     name: string;
   };
 
   export type Call = Node<"call"> & {
     method: Ref;
-    args: Array<Data | Ref | Call | Quest | List | Structure | Action | View | Component>;
+    args: Array<Entity>;
+  };
+
+  export type Expression = Node<"expression"> & {
+    method: Ref;
+    args: Array<Entity>;
   };
 
   export type Quest = Node<"quest"> & {
-    target: Data | Ref | Call;
+    target: Ref | Call | Expression;
+  };
+
+  export type Data = Node<"data"> & {
+    value: any;
   };
 
   export type List = Node<"list"> & {
-    args: Array<Data | Ref | Call | Quest | List | Structure | Action | View | Component>;
+    args: Array<Entity>;
   };
 
   export type Structure = Node<"structure"> & {
@@ -53,8 +62,8 @@ export namespace Abstract {
 
   export type Action = Node<"action"> & {
     parameters: Array<Parameter>;
-    statements: Array<Constant | Variable | Assignment | Quest>;
-    result: Data | Ref | Call | Quest | List | Structure | Action | View | Component;
+    statements: Array<Constant | Variable | Assignment | Entity>;
+    result: Entity;
   };
 
   export type View = Node<"view"> & {
@@ -63,17 +72,17 @@ export namespace Abstract {
   };
 
   export type Component = Node<"component"> & {
-    tagName: string;
+    name: string;
     attributes: Array<Attribute>;
   };
 
   export type Attribute = Node<"attribute"> & {
     name: string;
-    binding: Data | Ref | Call | Quest | List | Structure | Action | View | Component;
+    binding: Entity;
   };
 
   export type Assignment = Node<"assignment"> & {
     name: string;
-    binding: Data | Ref | Call | Quest | List | Structure | Action | View | Component;
+    binding: Entity;
   };
 }
--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,158 @@
 import { Abstract } from "./abstract";
 
-// TODO
+export class App {
+  readonly source: Abstract.App;
+  private readonly members: Record<string, Property | Constant | Variable> = {};
+
+  constructor(source: Abstract.App) {
+    this.source = source;
+
+    for (const member of source.members) {
+      if (member.kind === "property") {
+        this.members[member.parameter.name] = new Property(member);
+      } else if (member.kind === "constant") {
+        this.members[member.parameter.name] = new Constant(member);
+      } else if (member.kind === "variable") {
+        this.members[member.parameter.name] = new Variable(member);
+      }
+    }
+  }
+}
+
+interface Subscriber<Incoming = unknown> {
+  handleEvent(event: Incoming): void;
+}
+
+abstract class Publisher<Outgoing = unknown> {
+  private readonly listeners: Subscriber<Outgoing>[] = [];
+  private lastPublishedValue?: Outgoing;
+
+  addEventListener(listener: Subscriber<Outgoing>): void {
+    if (this.lastPublishedValue !== undefined) {
+      listener.handleEvent(this.lastPublishedValue);
+    }
+
+    this.listeners.push(listener);
+  }
+
+  protected publish(nextValue: Outgoing): void {
+    if (this.lastPublishedValue === nextValue) {
+      return;
+    }
+
+    this.lastPublishedValue = nextValue;
+
+    for (const listener of this.listeners) {
+      listener.handleEvent(nextValue);
+    }
+  }
+}
+
+abstract class Vertex<Type = unknown> extends Publisher<Type> implements Subscriber<Type> {
+  handleEvent(event: Type): void {
+    this.publish(event);
+  }
+}
+
+class Property extends Vertex {
+  readonly source: Abstract.Property;
+  private readonly parameter: Parameter;
+  private readonly binding: Entity;
+
+  constructor(source: Abstract.Property, binding: Entity) {
+    super();
+
+    this.source = source;
+    this.parameter = new Parameter(source.parameter);
+    this.binding = binding;
+  }
+
+  handleEvent(event: any) {
+    this.publish(event);
+  }
+}
+
+class Constant extends Vertex {
+  readonly source: Abstract.Constant;
+  private readonly parameter: Parameter;
+  private readonly binding: Entity;
+
+  constructor(source: Abstract.Constant) {
+    super();
+
+    this.source = source;
+    this.parameter = new Parameter(source.parameter);
+    this.binding = new Entity(source.binding);
+  }
+
+  handleEvent(event: any) {
+    this.publish(event);
+  }
+}
+
+class Variable extends Vertex {
+  readonly source: Abstract.Variable;
+  private readonly parameter: Parameter;
+  private binding: Entity;
+
+  constructor(source: Abstract.Variable) {
+    super();
+
+    this.source = source;
+    this.parameter = new Parameter(source.parameter);
+    this.binding = new Entity(source.binding);
+  }
+
+  handleEvent(event: any) {
+    this.publish(event);
+  }
+
+  setBinding(binding: Entity) {
+    this.binding = binding;
+  }
+}
+
+class Parameter {
+  readonly source: Abstract.Parameter;
+
+  constructor(source: Abstract.Parameter) {
+    this.source = source;
+  }
+}
+
+class Entity extends Publisher {
+  readonly source: Abstract.Entity;
+  private readonly binding: Ref | Call | Expression | Quest | Data | List | Structure | Action | View | Component;
+
+  constructor(source: Abstract.Entity) {
+    super();
+
+    this.source = source;
+
+    if (source.binding.kind === "ref") {
+      this.binding = new Ref(source.binding);
+    } else if (source.binding.kind === "call") {
+      this.binding = new Call(source.binding);
+    } else if (source.binding.kind === "expression") {
+      this.binding = new Expression(source.binding);
+    } else if (source.binding.kind === "quest") {
+      this.binding = new Quest(source.binding);
+    } else if (source.binding.kind === "data") {
+      this.binding = new Data(source.binding);
+    } else if (source.binding.kind === "list") {
+      this.binding = new List(source.binding);
+    } else if (source.binding.kind === "structure") {
+      this.binding = new Structure(source.binding);
+    } else if (source.binding.kind === "action") {
+      this.binding = new Action(source.binding);
+    } else if (source.binding.kind === "view") {
+      this.binding = new View(source.binding);
+    } else if (source.binding.kind === "component") {
+      this.binding = new Component(source.binding);
+    } else {
+      throw new Error(); // TODO: Add error message.
+    }
+  }
+}
+
+// TODO ...
